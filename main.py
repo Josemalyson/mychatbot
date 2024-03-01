12e1f4eb2e7a06b391e8abe031ede5dee1b8ad61
Josemalyson Oliveira <josemalyson@gmail.com>
2024-03-01 08:33:18 -0300

    add feature load docs

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import os
-import time
+import tempfile
 from io import StringIO
 
 import streamlit as st
 from dotenv import load_dotenv
-from langchain.callbacks import streaming_stdout
 from langchain.chains import LLMChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import UnstructuredURLLoader, YoutubeLoader, UnstructuredFileLoader
 from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
 from langchain_community.llms.ollama import Ollama
-from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler, streaming_stdout
+from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
-from openai import OpenAI
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from streamlit_js_eval import streamlit_js_eval
 
 load_dotenv()
-openai_api_key = os.getenv('OPENAI_API_KEY')
 model = os.getenv('MODEL')
-client = OpenAI(api_key=openai_api_key)
+openai_api_key = os.getenv('OPENAI_API_KEY')
 huggingfacehub_api_token = os.getenv('HUGGINGFACEHUB_API_TOKEN')
-
 repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"  # See https://huggingface.co/models?pipeline_tag=text-generation&sort=downloads for some other options
 
 # App title
@@ -40,12 +41,17 @@ with st.sidebar:
 
     elif selected_model == 'OpenAI':
         llm = ChatOpenAI(temperature=temperature, model=model, openai_api_key=openai_api_key, streaming=True)
+
     elif selected_model == 'Mistral':
         callbacks = [streaming_stdout.StreamingStdOutCallbackHandler()]
         llm = HuggingFaceEndpoint(
             repo_id=repo_id, temperature=temperature,
             huggingfacehub_api_token=huggingfacehub_api_token, callbacks=callbacks,
         )
+
+    uploaded_url = st.text_input("Choose Site URL")
+    uploaded_youtube_url = st.text_input("Choose Youtube URL")
+    uploaded_file = st.file_uploader("Choose a file")
 
     st.markdown(
         '📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
@@ -60,6 +66,10 @@ for message in st.session_state.messages:
         st.write(message["content"])
 
 
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
 def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "Fala ai Zé o que tu quer hoje ?"}]
     streamlit_js_eval(js_expressions="parent.window.location.reload()")
@@ -68,12 +78,66 @@ def clear_chat_history():
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
 
+def load_url_docs():
+    # Load, chunk and index the contents of the blog.
+    loader = UnstructuredURLLoader(urls=[uploaded_url])
+    docs = loader.load()
+
+    print(docs)
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+    # Retrieve and generate using the relevant snippets of the blog.
+    return vectorstore.as_retriever()
+
+
+def load_url_youtube_docs():
+    loader = YoutubeLoader.from_youtube_url(
+        uploaded_youtube_url,
+        add_video_info=True,
+        language=["pt"],
+        translation="pt",
+    )
+    docs = loader.load()
+
+    print(docs)
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+    # Retrieve and generate using the relevant snippets of the blog.
+    return vectorstore.as_retriever()
+
+
+def load_files():
+    # Create tmp file
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
+    with open(temp_filepath, "wb") as f:
+        f.write(uploaded_file.getvalue())
+    loader = UnstructuredFileLoader(temp_filepath)
+    docs = loader.load()
+
+    print(docs)
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+    vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+    # Retrieve and generate using the relevant snippets of the blog.
+    return vectorstore.as_retriever()
+
+
 def response_ollama(question):
     template = """
 
-    <<SYS>> Você é um assistente pessoal de AI. <</SYS>>
-    [INST] Responda todas as perguntas de forma simples e objetivo no idoma português. Ao final de cada resposta adicione o modelo
-        que você é exemplos: gpt-turbo, ollama e entre outros.
+    <<SYS>>Você é um assistenten pessoal que responde no idioma português do Brasil as perguntas do usuário, 
+        onde o mesmo enviaria uma questão e você entenderá o contexto enviado e com palavras simples explicará com 
+        detalhes a reposta. <</SYS>>
+    [INST]
         {question} 
     [/INST]
 
@@ -84,35 +148,63 @@ def response_ollama(question):
                                      template_format='f-string', validate_template=True)
 
     llm_chain = LLMChain(prompt=prompt_template, llm=llm)
-    response_llm = llm_chain.run({"question": question})
-
-    for word in response_llm.split():
-        yield word + " "
-        time.sleep(0.05)
+    response_llm = llm_chain.invoke({"question": question})
+    return response_llm['text']
 
 
-def response_chatgpt():
-    # default chat without context
-    stream = client.chat.completions.create(
-        model=model,
-        messages=[
-            {"role": m["role"], "content": m["content"]}
-            for m in st.session_state.messages
-        ],
-        stream=True,
+def response_chatgpt(question, retrievers):
+    template = (
+        """Você é um assistenten pessoal que responde no idioma português do Brasil as perguntas do usuário, 
+        onde o mesmo enviaria uma questão e você entenderá o contexto enviado e com palavras simples explicará com 
+        detalhes a reposta.
+        
+        Question: {question} 
+        
+        Context: {context} 
+        
+        Answer: Responda com educação e de forma simples.
+        """
+
     )
-    return st.write_stream(stream)
+
+    prompt_template = PromptTemplate(input_variables=['question', 'context'], output_parser=None, partial_variables={},
+                                     template=template,
+                                     template_format='f-string', validate_template=True)
+
+    rag_chain = (
+            {"context": retrievers | format_docs, "question": RunnablePassthrough()}
+            | prompt_template
+            | llm
+            | StrOutputParser()
+    )
+
+    response_gtp = rag_chain.invoke(question)
+
+    return response_gtp
 
 
 def response_huggingface(question):
-    return llm(question)
+    template = (
+        """Você é um assistenten pessoal que responde no idioma português do Brasil as perguntas do usuário, 
+        onde o mesmo enviaria uma questão e você entenderá o contexto enviado e com palavras simples explicará com 
+        detalhes a reposta.
+
+        Pergunta: {question}
+        Answer: Responda com educação e de forma simples.
+        """
+
+    )
+
+    prompt_template = PromptTemplate(input_variables=['question'], output_parser=None, partial_variables={},
+                                     template=template,
+                                     template_format='f-string', validate_template=True)
+    llm_chain = LLMChain(prompt=prompt_template, llm=llm)
+    response_hf = llm_chain.invoke(question)
+
+    return response_hf['text']
 
 
-uploaded_url = st.text_input("Choose Site URL")
-uploaded_youtube_url = st.text_input("Choose Youtube URL")
-uploaded_file = st.file_uploader("Choose a file")
-
-if uploaded_file is not None:
+if uploaded_url is not None and uploaded_url != '':
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
     st.write(bytes_data)
@@ -142,23 +234,20 @@ if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
 
+            if uploaded_url is not None and uploaded_url != '':
+                retriever = load_url_docs()
+
+            if uploaded_youtube_url is not None and uploaded_youtube_url != '':
+                retriever = load_url_youtube_docs()
+
+            if uploaded_file is not None and uploaded_file != '':
+                retriever = load_files()
+
             if selected_model == 'Llama2':
                 response = response_ollama(prompt)
-                placeholder = st.empty()
-                full_response = ''
-                for item in response:
-                    full_response += item
-                    placeholder.markdown(full_response)
-                placeholder.markdown(full_response)
-                message = {"role": "assistant", "content": full_response}
-
             elif selected_model == 'OpenAI':
-                response = response_chatgpt()
-                message = {"role": "assistant", "content": response}
-
+                response = response_chatgpt(prompt, retriever)
             elif selected_model == 'Mistral':
                 response = response_huggingface(prompt)
-                message = {"role": "assistant", "content": response}
-                st.write(response)
-
-    st.session_state.messages.append(message)
+        st.write(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
