b9f279db0f04711e3f7f3e6fa8d4222c7f8f896d
Josemalyson Oliveira <josemalyson@gmail.com>
2024-03-02 18:12:48 -0300

    refactory files

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import tempfile
 
 import streamlit as st
 from dotenv import load_dotenv
-from langchain import hub
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.chat_message_histories import StreamlitChatMessageHistory
 from langchain_community.chat_models import ChatOllama
@@ -19,81 +18,87 @@ from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from streamlit_js_eval import streamlit_js_eval
 
+# load envs
 load_dotenv()
+
+# envs
 model = os.getenv('MODEL')
 openai_api_key = os.getenv('OPENAI_API_KEY')
 humbugging_api_token = os.getenv('HUGGINGFACEHUB_API_TOKEN')
-repo_id = "mistralai/Mixtral-8x7B-Instruct-v0.1"
+repo_id = os.getenv('REPO_ID')
+model_ollama = os.getenv('MODEL_OLLAMA')
+chunk_size = os.getenv('CHUNK_SIZE')
+chunk_overlap = os.getenv('CHUNK_OVERLAP')
+
 docs = [Document(page_content=' none ')]
-history = StreamlitChatMessageHistory(key="chat_messages")
+history = StreamlitChatMessageHistory(key="chat_history")
 chat_history = []
-# App title
-st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 
-with st.sidebar:
-    st.title('🦙💬 Llama 2 Chatbot')
-    st.subheader('Models and parameters')
-    selected_model = st.sidebar.selectbox('Choose a model', ['Gemma', 'OpenAI', 'Mistral'],
-                                          key='selected_model')
+# create tittle
+st.set_page_config(page_title="💻 🖱️ Zé Chatbot")
 
-    temperature = st.sidebar.slider('temperature', min_value=0.1, max_value=1.0, value=0.1, step=0.1)
 
-    if selected_model == 'Gemma':
-        llm = ChatOllama(model='neural-chat', base_url='http://localhost:11434',
-                         callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
-                         , temperature=temperature)
-        prompt = hub.pull("rlm/rag-prompt-llama")
+def create_llm():
+    if selected_model == 'OpenAI':
+        return ChatOpenAI(temperature=temperature, model=model, openai_api_key=openai_api_key, streaming=True)
 
-    elif selected_model == 'OpenAI':
-        llm = ChatOpenAI(temperature=temperature, model=model, openai_api_key=openai_api_key, streaming=True)
-        prompt = hub.pull("rlm/rag-prompt")
-    elif selected_model == 'Mistral':
+    elif selected_model == 'Nvidia - HF':
         callbacks = [streaming_stdout.StreamingStdOutCallbackHandler()]
-        llm = HuggingFaceEndpoint(
+        return HuggingFaceEndpoint(
             repo_id=repo_id, temperature=temperature,
             huggingfacehub_api_token=humbugging_api_token, callbacks=callbacks,
         )
-        prompt = hub.pull("rlm/rag-prompt-mistral")
 
-    uploaded_url = st.text_input("Choose Site URL")
-    uploaded_youtube_url = st.text_input("Choose Youtube URL")
+    elif selected_model == 'Llama2 - Local':
+        return ChatOllama(model=model_ollama, base_url='http://localhost:11434',
+                          callback_manager=CallbackManager([StreamingStdOutCallbackHandler()])
+                          , temperature=temperature)
+
+
+# create sidebar
+
+with st.sidebar:
+    # create title
+    st.title('💻 🖱️ Zé Chatbot')
+    st.subheader('Models and parameters')
+
+    # create select
+    selected_model = st.sidebar.selectbox('Choose a model', ['OpenAI', 'Nvidia - HF', 'Llama2 - Local'],
+                                          key='selected_model')
+
+    # config temperratue
+    temperature = st.sidebar.slider('temperature', min_value=0.1, max_value=1.0, value=0.1, step=0.1)
+
+    # config model llm
+    llm = create_llm()
+
+    # create components upload
+    site_url = st.text_input("Choose Site URL")
+    youtube_url = st.text_input("Choose Youtube URL")
     uploaded_file = st.file_uploader("Choose a file")
-    # image = st.file_uploader("Choose a Image")
 
     st.markdown(
         '📖 Learn how to build this app in this [blog](https://blog.streamlit.io/how-to-build-a-llama-2-chatbot/)!')
 
-# # Store LLM generated responses
-# if "messages" not in st.session_state.keys():
-#     st.session_state.messages = [{"role": "assistant", "content": "Fala ai Zé o que tu quer hoje ?"}]
-#
-# # Display or clear chat messages
-# for message in st.session_state.messages:
-#     with st.chat_message(message["role"]):
-#         st.write(message["content"])
 
-if len(history.messages) == 0:
-    history.add_ai_message("Fala ai Zé o que tu quer hoje ?")
-
-for msg in history.messages:
-    st.chat_message(msg.type).write(msg.content)
-
-
-def format_docs(data):
-    return "\n\n".join(doc.page_content for doc in data)
-
-
+# clean and reload page
 def clear_chat_history():
-    st.session_state.messages = [{"role": "assistant", "content": "Fala ai Zé o que tu quer hoje ?"}]
     streamlit_js_eval(js_expressions="parent.window.location.reload()")
 
 
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
-contextualize_q_system_prompt = """Given a chat history and the latest user question \
-which might reference context in the chat history, formulate a standalone question \
-which can be understood without the chat history. Do NOT answer the question, \
-just reformulate it if needed and otherwise return it as is."""
+
+# format documents in text format
+def format_docs(data):
+    return "\n\n".join(doc.page_content for doc in data)
+
+
+# create default prompt with chat history
+contextualize_q_system_prompt = """Dado um histórico de bate-papo e a última pergunta do usuário \
+que pode fazer referência ao contexto no histórico do bate-papo, formule uma pergunta independente \
+que pode ser entendido sem o histórico de bate-papo. NÃO responda à pergunta, \
+apenas reformule-o se necessário e devolva-o como está. responda sempre em português do Brasil."""
 contextualize_q_prompt = ChatPromptTemplate.from_messages(
     [
         ("system", contextualize_q_system_prompt),
@@ -101,176 +106,110 @@ contextualize_q_prompt = ChatPromptTemplate.from_messages(
         ("human", "{question}"),
     ]
 )
+
+# create chain with LCE
 contextualize_q_chain = contextualize_q_prompt | llm | StrOutputParser()
 
+# create Load, chunk and index the contents
+if site_url is not None and site_url != '':
+    loader = UnstructuredURLLoader(urls=[site_url])
+    docs = loader.load()
+    expander = st.expander("Contexto Adicionado")
+    expander.write(format_docs(docs))
 
-def contextualized_question(input: dict):
-    if input.get("chat_history"):
+# create Load, chunk and index the contents
+if youtube_url is not None and youtube_url != '':
+    loader = YoutubeLoader.from_youtube_url(
+        youtube_url,
+        add_video_info=True,
+        language=["pt"],
+        translation="pt",
+    )
+    docs = loader.load()
+    expander = st.expander("Contexto Adicionado")
+    expander.write(format_docs(docs))
+
+# create Load, chunk and index the contents
+if uploaded_file is not None and uploaded_file != '':
+    # Create tmp file
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
+    with open(temp_filepath, "wb") as f:
+        f.write(uploaded_file.getvalue())
+    loader = UnstructuredFileLoader(temp_filepath)
+    docs = loader.load()
+    expander = st.expander("Contexto Adicionado")
+    expander.write(format_docs(docs))
+
+# show messages chat
+for msg in history.messages:
+    st.chat_message(msg.type).write(msg.content)
+
+
+# logic to call prompt default without context
+def contextualized_question(data: dict):
+    if data.get("chat_history"):
         return contextualize_q_chain
     else:
-        return input["question"]
+        return data["question"]
 
 
+# get question of user
 if question := st.chat_input():
     st.chat_message("human").write(question)
 
-    if uploaded_url is not None and uploaded_url != '':
-        loader = UnstructuredURLLoader(urls=[uploaded_url])
-        docs = loader.load()
-
-    if uploaded_youtube_url is not None and uploaded_youtube_url != '':
-        loader = YoutubeLoader.from_youtube_url(
-            uploaded_youtube_url,
-            add_video_info=True,
-            language=["pt"],
-            translation="pt",
-        )
-        docs = loader.load()
-
-    if uploaded_file is not None and uploaded_file != '':
-        # Create tmp file
-        temp_dir = tempfile.TemporaryDirectory()
-        temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
-        with open(temp_filepath, "wb") as f:
-            f.write(uploaded_file.getvalue())
-        loader = UnstructuredFileLoader(temp_filepath)
-        docs = loader.load()
-
-    # if image is not None and image != '':
-    #     # Create tmp file
-    #     temp_dir = tempfile.TemporaryDirectory()
-    #     temp_filepath = os.path.join(temp_dir.name, image.name)
-    #     with open(temp_filepath, "wb") as f:
-    #         f.write(image.getvalue())
-    #     loader = UnstructuredImageLoader(temp_filepath, mode="elements")
-    #     docs = loader.load()
-
-    print(docs)
-
+    # create SPLIT
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     splits = text_splitter.split_documents(docs)
 
+    # Store splits
     vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
+
+    # create STORE
     retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
 
-    qa_system_prompt = """You are an assistant for question-answering tasks. \
-    Use the following pieces of retrieved context to answer the question. \
-    If you don't know the answer, just say that you don't know. \
-    Use three sentences maximum and keep the answer concise.\
+    # create template with context
+    qa_system_prompt = """Você é um assistente para tarefas de resposta a perguntas. \
+     Use as seguintes partes do contexto recuperado para responder à pergunta. \
+     Se você não sabe a resposta, basta dizer que não sabe. \
+     Use no máximo três frases e mantenha a resposta concisa.  responda sempre em português do Brasil.
 
     {context}"""
     qa_prompt = ChatPromptTemplate.from_messages(
         [
-            ("system", qa_system_prompt),
-            MessagesPlaceholder(variable_name="chat_history"),
+            ("system", qa_system_prompt), MessagesPlaceholder(variable_name="chat_history"),
             ("human", "{question}"),
         ]
     )
 
+    # RetrievalQA
     rag_chain = (
             RunnablePassthrough.assign(
                 context=contextualized_question | retriever | format_docs
             )
-            | prompt
+            | qa_prompt
             | llm
+            | StrOutputParser()
     )
 
+    # Chain add chat history
     chain_with_history = RunnableWithMessageHistory(
         rag_chain,
-        lambda session_id: history,  # Always return the instance created earlier
+        lambda session_id: history,
         input_messages_key="question",
         history_messages_key="chat_history",
     )
 
     config = {"configurable": {"session_id": "any"}}
-    response = chain_with_history.invoke({"question": question}, config)
-    if response is not None and hasattr(response, 'content'):
-        st.chat_message("ai").write(response.content)
+
+    # chain with stream active
+    answer = chain_with_history.stream({"question": question}, config)
+
+    # choice response to difference models of llm
+    if answer is not None and hasattr(answer, 'content'):
+        st.chat_message("ai").write_stream(answer.content)
     else:
-        st.chat_message("ai").write(response)
+        st.chat_message("ai").write_stream(answer)
 
+    # clean vector database
     vectorstore.delete_collection()
-
-# def rag(question, retrievers):
-#     template = """Você é um assistente para tarefas de resposta a perguntas. Use as seguintes partes do contexto
-#     recuperado para responder à pergunta. Se você não sabe a resposta, basta dizer que não sabe. Use frases e
-#     mantenha a resposta concisa.
-#
-#     Question: {question}
-#
-#     Context: {context}
-#
-#     Answer:
-#     """
-#
-#     prompt_template = PromptTemplate(input_variables=['question', 'context'], output_parser=None, partial_variables={},
-#                                      template=template,
-#                                      template_format='f-string', validate_template=True)
-#
-#     # prompt default in https://smith.langchain.com/hub
-#     # prompt_template = hub.pull("rlm/rag-prompt")
-#
-#     rag_chain = (
-#             {"context": retrievers | format_docs, "question": RunnablePassthrough()}
-#             | prompt_template
-#             | llm
-#             | StrOutputParser()
-#     )
-#
-#     return rag_chain.invoke(question)
-
-#
-# # User-provided prompt
-# if prompt := st.chat_input():
-#     st.session_state.messages.append({"role": "user", "content": prompt})
-#     with st.chat_message("user"):
-#         st.write(prompt)
-#
-# if st.session_state.messages[-1]["role"] != "assistant":
-#     with st.chat_message("assistant"):
-#         with st.spinner("Thinking..."):
-#
-#             if uploaded_url is not None and uploaded_url != '':
-#                 loader = UnstructuredURLLoader(urls=[uploaded_url])
-#                 docs = loader.load()
-#
-#             if uploaded_youtube_url is not None and uploaded_youtube_url != '':
-#                 loader = YoutubeLoader.from_youtube_url(
-#                     uploaded_youtube_url,
-#                     add_video_info=True,
-#                     language=["pt"],
-#                     translation="pt",
-#                 )
-#                 docs = loader.load()
-#
-#             if uploaded_file is not None and uploaded_file != '':
-#                 # Create tmp file
-#                 temp_dir = tempfile.TemporaryDirectory()
-#                 temp_filepath = os.path.join(temp_dir.name, uploaded_file.name)
-#                 with open(temp_filepath, "wb") as f:
-#                     f.write(uploaded_file.getvalue())
-#                 loader = UnstructuredFileLoader(temp_filepath)
-#                 docs = loader.load()
-#
-#             # if image is not None and image != '':
-#             #     # Create tmp file
-#             #     temp_dir = tempfile.TemporaryDirectory()
-#             #     temp_filepath = os.path.join(temp_dir.name, image.name)
-#             #     with open(temp_filepath, "wb") as f:
-#             #         f.write(image.getvalue())
-#             #     loader = UnstructuredImageLoader(temp_filepath, mode="elements")
-#             #     docs = loader.load()
-#
-#             print(docs)
-#
-#             text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-#             splits = text_splitter.split_documents(docs)
-#
-#             vectorstore = Chroma.from_documents(documents=splits, embedding=OpenAIEmbeddings())
-#             retriever = vectorstore.as_retriever()
-#
-#             response = rag(prompt, retriever)
-#             vectorstore.delete_collection()
-#
-#         st.write(response)
-#     st.session_state.messages.append({"role": "assistant", "content": response})
